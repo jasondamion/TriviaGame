c6c61cc9b29c3eb531a9605ae7f390cbe3dc0a8d
Jason Nelson <Jasonnelson11@gmail.com>
2018-12-18 18:30:44 -0500

    Fixed the start quiz button

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// const squiz = document.getElementById('start');
+// squiz.addEventListener('click', starting());
+
+function starting(){
+   
+    document.getElementById("start").style.display = "none";
+    document.getElementById("whole").style.display = "inline";
+}
